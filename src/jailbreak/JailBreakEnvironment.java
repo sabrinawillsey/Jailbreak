9656a007c10c6ab33402cd922d22cd72b57c8c4f
Thomas King <thomas.king@brentwood.bc.ca>
2013-05-03 13:30:52 -0700

    Complete changes from Thomas

--- src/jailbreak/JailBreakEnvironment.java
+++ src/jailbreak/JailBreakEnvironment.java
@@ -1,48 +1,108 @@
 package jailbreak;
 
+import JailBreak.Prisoner;
 import environment.Direction;
 import environment.Environment;
 import environment.GraphicsPalette;
 import environment.Grid;
+import environment.Velocity;
+import image.ResourceTools;
 import java.awt.Color;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.Image;
+import java.awt.Point;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
 
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-/**
- *
- * @author Sabrina
- */
 public class JailBreakEnvironment extends Environment {
 
-    private Grid grid;
+    private int backgroundSpeed = 0;
+    private Prisoner prisoner;
+    private long jumpStartTimeMillis;
+    private boolean jumping;
 
     public JailBreakEnvironment(Image background) {
         super(background);
+    }
 
+    @Override
+    public void paintBackground(Graphics2D graphics2D) {
+        graphics2D.drawImage(this.getBackgroundImage(), this.getBackgroundImagePosition().x, this.getBackgroundImagePosition().y, this);
+        graphics2D.drawImage(this.getBackgroundImage(), this.getBackgroundImagePosition().x + this.getBackgroundImage().getWidth(this), this.getBackgroundImagePosition().y, this);
     }
 
     @Override
     public void initializeEnvironment() {
-        
+
+        this.prisoner = new Prisoner(ResourceTools.loadImageFromResource("resources/prisoner 3.png"), new Point(525, 325), new Velocity(0, 0));
+        this.getActors().add(prisoner);
     }
 
     @Override
     public void timerTaskHandler() {
+        this.getBackgroundImagePosition().x += this.backgroundSpeed;
+        //test if background needs to be reset
+        if (this.getBackgroundImagePosition().x <= -this.getBackgroundImage().getWidth(this)) {
+            this.getBackgroundImagePosition().x = 0;
+            System.out.println("reset");
+        }
+
+        //test if the prisoner is jumping, and stop fall if he is at or below the floor
+        if (prisoner.getPosition().y > 325) {
+            this.jumping = false;
+            this.prisoner.getPosition().y = 325;
+        }
+        if (prisoner.getPosition().y < 0) {
+            this.prisoner.getPosition().y = 0;
+        }
+        
+        //has he been in the air for more than 2 seconds?
+        if ((this.jumping ) && (System.currentTimeMillis() - this.jumpStartTimeMillis >= 600)){
+            this.prisoner.getVelocity().y = 20;
+        }
+        
+        
+        if (prisoner.getPosition().y <= 325) {
+        }
     }
 
     @Override
-    public void keyPressedHandler(KeyEvent e) {
+    public void keyPressedHandler(KeyEvent ke) {
+//        if (ke.getKeyCode() == KeyEvent.VK_RIGHT) {
+//            this.prisoner.getVelocity().x = 8;
+//        }
+//        if (ke.getKeyCode() == KeyEvent.VK_LEFT) {
+//            this.prisoner.getVelocity().x = -8;
+//        }
+        if (ke.getKeyCode() == KeyEvent.VK_S) {
+            this.backgroundSpeed = -15;
+        }
+        //       if (ke.getKeyCode() == KeyEvent.VK_LEFT) {
+        //           this.backgroundSpeed = +7;
+        //       }
+        if ((ke.getKeyCode() == KeyEvent.VK_UP) && (this.jumping == false)){
+            this.jumping = true;
+            this.jumpStartTimeMillis = System.currentTimeMillis();
+            this.prisoner.getVelocity().y = -30;
+        }
     }
 
     @Override
-    public void keyReleasedHandler(KeyEvent e) {
+    public void keyReleasedHandler(KeyEvent ke) {
+        if (ke.getKeyCode() == KeyEvent.VK_UP) {
+            this.prisoner.getVelocity().y = 30;
+        }
+//        if (ke.getKeyCode() == KeyEvent.VK_RIGHT) {
+//            this.prisoner.getVelocity().x = 0;
+//        }
+//        if (ke.getKeyCode() == KeyEvent.VK_LEFT) {
+//            this.prisoner.getVelocity().x = 0;
+//        }
+        //      if (ke.getKeyCode() == KeyEvent.VK_RIGHT) {
+        //          this.backgroundSpeed = 0;
+        //      }
     }
 
     @Override
--- src/jailbreak/Prisoner.java
+++ src/jailbreak/Prisoner.java
@@ -2,139 +2,21 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package jailbreak;
+package JailBreak;
 
-import environment.Direction;
-import environment.Grid;
-import image.ResourceTools;
-import java.awt.Color;
-import java.awt.Graphics;
+import environment.Actor;
+import environment.Velocity;
+import java.awt.Image;
 import java.awt.Point;
-import java.util.ArrayList;
 
-/**
- *
- * @author Sabrina
- */
-public class Prisoner {
-//    public void draw(Graphics graphics) {
-//                    graphics.drawImage(ResourceTools.loadImageFromResource("resources/prisoner.jpg"), 550, 100, 200, 300, Color.yellow, null);
-//    }
-//    private ArrayList<Point> body = new ArrayList<Point>();
-     public void move() {
-        if (this.body != null) {
-            this.body.remove(this.body.size() - 1);
+public class Prisoner extends Actor {
 
-//            if (this.direction == Direction.RIGHT) {
-//                this.body.add(0, new Point(getHead().x + 1, getHead().y));
-//            } else if (this.direction == Direction.LEFT) {
-//                this.body.add(0, new Point(getHead().x - 1, getHead().y));
-//            } 
-        }
-   }
-
-    public void grow(int increase) {
-        for (int i = 0; i < increase; i++) {
-            this.body.add(new Point(Integer.MIN_VALUE, Integer.MIN_VALUE));
-        }
+    public Prisoner(Image image, Point position, Velocity velocity) {
+        super(position, velocity);
+        this.setImage(image);
     }
 
-    public void shrink(int decrease) {
-        for (int i = 0; i < decrease; i++) {
-            this.body.remove(this.body.size()-1);
-        }
+    public Prisoner(Point position, Velocity velocity) {
+        super(position, velocity);
     }
-    public Point getHead() {
-        return this.body.get(HEAD_INDEX);
-    }
-
-    public boolean hitTest(ArrayList<Point> locations) {
-//          return true if the head of the snake intersects with any of the locations
-        for (Point location : locations) {
-            if (this.getHead().equals(location)) {
-                return true;
-                
-            }
-
-        }
-        return false;
-        
-      
-    }
-//    public void draw(Graphics graphics) {
-//        Point bodyPartLocation;
-//        if (body != null) {        //(0,1)
-//            graphics.setColor(color);
-//            for (int i = 0; i < body.size(); i++) {
-//                //draw each part of the snake
-////                bodyPartLocation = grid.getCellPosition(body.get(i));
-//                if (i == 0) {
-//                    //draw head
-//                    graphics.fillRect(bodyPartLocation.x, bodyPartLocation.y, 200, 300);
-//                } else { //draw body
-//                    graphics.setColor(this.color);
-//                    graphics.fillOval(bodyPartLocation.x, bodyPartLocation.y, 200, 300);
-//
-//                }
-//            }
-//        }
-//    }
-    //</editor-fold>
-// <editor-fold defaultstate="collapsed" desc="Constructors">    
-//    private Direction direction = Direction.RIGHT;
-    private final int HEAD_INDEX = 0;
-    private ArrayList<Point> body = new ArrayList<Point>();
-    private Color color = Color.RED;
-
-//    public ASnake(ArrayList<Point> body) {
-//        this.body = body;
-//    }
-// </editor-fold>    
-// <editor-fold defaultstate="collapsed" desc="Properties">
-
-    /**
-     * @return the body
-     */
-    public ArrayList<Point> getBody() {
-        return body;
-    }
-
-    /**
-     * @param body the body to set
-     */
-    public void setBody(ArrayList<Point> body) {
-        this.body = body;
-    }
-
-    /**
-     * @return the direction
-     */
-//    public Direction getDirection() {
-//        return direction;
-//    }
-//
-//    /**
-//     * @param direction the direction to set
-//     */
-//    public void setDirection(Direction direction) {
-//        this.direction = direction;
-//    }
-
-    /**
-     * @return the color
-     */
-    public Color getColor() {
-        return color;
-    }
-
-    /**
-     * @param color the color to set
-     */
-    public void setColor(Color color) {
-        this.color = color;
-    }
-    //  </editor-fold>
 }
-
-     
-//}
